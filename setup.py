92e0a87113faa6cb381d2eeae026d483823aec2a
Tony Hirst <tony.hirst@gmail.com>
2018-02-12 17:35:38 +0000

    base setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tm351-utils',
+      version='0.0.1',
+      description='Utilities for use in TM351 student notebook activities',
+      author='Tony Hirst',
+      author_email='tony.hirst@open.ac.uk',
+      license='MIT',
+      packages=['tm351_utils'],
+      install_requires=[],
+      zip_safe=False)
