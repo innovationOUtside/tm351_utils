f21ab5aa903424e8e0338f55cd08c32bdc9c95f7
Tony Hirst <tony.hirst@gmail.com>
2018-02-12 17:36:37 +0000

    First commit table_def()

--- tm351_utils/utils.py
+++ tm351_utils/utils.py
@@ -1,4 +1,30 @@
 
+
+#Run this cell to create the table_def() function to generate tidy output from pg_dump
+#You don't need to know how the Linux command line voodoo works...
+def table_def(table,db='tm351test', retval=False, noprint=False):
+    ''' Helper function defined to display a tidied up version 
+        of the table creation statements
+        created using pg_dump '''
+    
+    #The sed commands remove extraneous lines; the cat -s command removes doubled up blank lines
+    #Escape the quotes surrounding the table name so we can quote tables with eg underscores in name
+    tables = [table] if not isinstance(table, list) else table
+    tables = ''.join([' -t \"{}\" '.format(t) for t in tables ] )
+    #https://stackoverflow.com/a/7353141/454773
+    task = subprocess.Popen("pg_dump -s {} {} | sed  -e '/ *SET .*;/ d' | sed  -e '/-- Dumped .*/ d' | cat -s".format(tables,db), shell=True, stdout=subprocess.PIPE)
+    txt,err = task.communicate()
+    if not noprint: print(txt.decode('utf-8'))
+    if retval or noprint: return txt.decode('utf-8')
+
+#Alternative one liner as a shell command
+#!pg_dump -s -t $table $db | sed  -e '/ *SET .*;/ d' | sed  -e '/-- Dumped .*/ d' | sed '/^$/{N;/^\n$/d;}'
+
+
+
+
+
+
 #Diff two text strings and generate some coloured HTML to show the diffs
 
 #https://github.com/dsindex/blog/wiki/%5Bpython%5D-difflib,-show-differences-between-two-strings
